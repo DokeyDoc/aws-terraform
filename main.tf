c3e19e09e4abcb29c7be28064c273648032872d0
RoiVioli <franco.valentin.sn@gmail.com>
2025-02-10 19:13:36 +0000

    feat: add content create S3 with terraform

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "terraform-test" {
+resource "aws_instance" "terraform_test" {
   ami           = "ami-085ad6ae776d8f09c"
   instance_type = "t2.nano"
   key_name      = "vockey"
@@ -10,3 +10,13 @@ resource "aws_instance" "terraform-test" {
     Name = "terraform-test"
   }
 }
+
+resource "aws_s3_bucket" "terraform_s3" {
+  bucket = "terraform-test-bucket-roivioli" 
+
+  tags = {
+    Name        = "terraform-s3"
+    Environment = "dev"
+  }
+}
+
