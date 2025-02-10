3f9c4f8e7988380d9dbd46fe838b95e46a733e19
RoiVioli <franco.valentin.sn@gmail.com>
2025-02-10 18:07:41 +0000

    feat: test create isntance ec2 aws

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
-provider "local" {}
+provider "aws" {
+  region = "us-east-1"
+}
 
 resource "local_file" "foo" {
-  content  = "Hello DevOps !"
-  filename = "${path.module}/devops.txt"
+  content         = "Hello DevOps !"
+  filename        = "${path.module}/devops.txt"
   file_permission = "0644"
 }
