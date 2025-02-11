cbc459395da2d3206896c4f9d55e3e1da6de6650
RoiVioli <franco.valentin.sn@gmail.com>
2025-02-11 22:55:39 +0000

    feat: add vars terraform

--- main.tf
+++ main.tf
@@ -2,9 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "ec2_type" {
+  description = "Le type d'instance qu'on souhaite"
+  type        = string
+  default     = "t4g.nano"
+}
+
 resource "aws_instance" "terraform_test" {
-  ami           = "ami-085ad6ae776d8f09c"
-  instance_type = "t2.nano"
+  ami           = "ami-0e532fbed6ef00604"
+  instance_type = var.ec2_type
   key_name      = "vockey"
   tags = {
     Name = "terraform-test"
@@ -12,7 +18,7 @@ resource "aws_instance" "terraform_test" {
 }
 
 resource "aws_s3_bucket" "terraform_s3" {
-  bucket = "terraform-test-bucket-roivioli" 
+  bucket = "terraform-test-bucket-roivioli"
 
   tags = {
     Name        = "terraform-s3"
@@ -20,3 +26,10 @@ resource "aws_s3_bucket" "terraform_s3" {
   }
 }
 
+output "public_ip" {
+  value = aws_instance.terraform_test.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.terraform_test.private_ip
+}
